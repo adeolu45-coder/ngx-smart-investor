2eeca10b0a6106a159e67c365f908e7147d1865e
adeolu45-coder <adeolu45@gmail.com>
2026-03-25 12:15:03 +0100

    Update server.py

--- server.py
+++ server.py
@@ -79,8 +79,11 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://ngx-frontend-i8nw.onrender.com",
+        "http://localhost:3000",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
